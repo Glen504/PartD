2412b8cd5564aaa289b769ea7b11c37dbf601f59
glen504 <glen.hart@outlook.co.nz>
2019-12-19 10:39:33 +1300

    First line

---
+++ src/MyNameIs.java
@@ -0,0 +1,13 @@
+
+public class MyNameIs {
+
+	public MyNameIs() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hi, my name is Glen");
+
+	}
+
+}
